7781ece0c51833705ee2b041ed7e55385358f80b
Vikil1001 <vincent.kilanowski@gmail.com>
2023-01-26 12:08:27 -0500

    update ville

--- include/Ville.h
+++ include/Ville.h
@@ -2,16 +2,33 @@
 #define VILLE_H
 
 #include <iostream>
-//#include <batiment.h>
+#include "batiment.h"
 #include "terrain.h"
+#include "structRessources.h"
 
 class Ville
 {
 private:
-    //RessourcesTotal ressourceTotal;
-    //Terrain* gridT = new Terrain*[][];
+    Ressources ressourceTotal;
+    Terrain* gridT[100][100];
+    Batiment* gridB[100][100];
+    int temps;
+
+    void calculEnergie();
+    void calculProd();
+    void calculEau();
+    void calculBonheur();
+    void calculMaterieux();
+    void calculArgent();
+    void calculHabitant();
+    void catastrophe();
+    bool isConstructible();
 public:
     Ville();
     ~Ville();
+
+    void construireBatiment(int x, int y);
+    void construireRoute(int x, int y);
+    void detruire(int x, int y);
 };
 #endif
