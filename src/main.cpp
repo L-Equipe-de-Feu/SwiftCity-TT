ed01fdfc114639fee9f7b9be92a14f35e67a1b55
Mat-desau <115102447+Mat-desau@users.noreply.github.com>
2023-01-26 09:54:54 -0500

    Arduino1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
-using namespace std;
+#include <Arduino.h>
 
-int main(int argc, char *argv[])
-{
-	cout << "Hello World!!";
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
 }
