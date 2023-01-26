0d86b338c17db0b9028deec89a7ea5d22723965f
Mat-desau <115102447+Mat-desau@users.noreply.github.com>
2023-01-26 10:55:45 -0500

    Arduino2

---
+++ include/variables.h
@@ -0,0 +1,77 @@
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
+
+#include <Arduino.h>
+
+//ACCELEROMÈETRE
+#ifndef X_AXIS_PIN
+#define X_AXIS_PIN A3
+#endif
+
+#ifndef Y_AXIS_PIN
+#define Y_AXIS_PIN A4
+#endif
+
+#ifndef Z_AXIS_PIN
+#define Z_AXIS_PIN A2
+#endif
+
+
+//JOYSTICK
+#ifndef JOY_GD_PIN
+#define JOY_GD_PIN A1
+#endif
+
+#ifndef JOY_HB_PIN
+#define JOY_HB_PIN A0
+#endif
+
+
+//BOUTTONS
+#ifndef BOU_A_PIN
+#define BOU_A_PIN 30
+#endif
+
+#ifndef BOU_B_PIN
+#define BOU_B_PIN 31
+#endif
+
+#ifndef BOU_M_PIN
+#define BOU_M_PIN 29
+#endif
+
+#ifndef BOU_S_PIN
+#define BOU_S_PIN 28
+#endif
+
+#ifndef BOU_ARD_PIN
+#define BOU_ARD_PIN 27
+#endif
+
+#ifndef BOU_ARG_PIN
+#define BOU_ARG_PIN 26
+#endif
+
+
+//LEDS
+#ifndef LED_1
+#define LED_1 22
+#endif
+
+#ifndef LED_2
+#define LED_2 23
+#endif
+
+#ifndef LED_3
+#define LED_3 24
+#endif
+
+#ifndef LED_4
+#define LED_4 25
+#endif
+
+
+//7 SEGMENTS
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,69 @@
 #include <Arduino.h>
+#include "variables.h"
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(9600);
+    pinMode(X_AXIS_PIN, INPUT);
+    pinMode(Y_AXIS_PIN, INPUT);
+    pinMode(Z_AXIS_PIN, INPUT);
+
+    pinMode(JOY_GD_PIN, INPUT);
+    pinMode(JOY_HB_PIN, INPUT);
+    
+    pinMode(BOU_A_PIN, INPUT);
+    pinMode(BOU_B_PIN, INPUT);
+    pinMode(BOU_M_PIN, INPUT);
+    pinMode(BOU_S_PIN, INPUT);
+    pinMode(BOU_ARG_PIN, INPUT);
+    pinMode(BOU_ARD_PIN, INPUT);
+
+    pinMode(LED_1, OUTPUT);
+    pinMode(LED_2, OUTPUT);
+    pinMode(LED_3, OUTPUT);
+    pinMode(LED_4, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if( digitalRead(BOU_A_PIN) == HIGH){
+    Serial.print("Boutton A cliquer \n");
+    Serial.println("Allumange de la LED1");
+    digitalWrite(LED_1, HIGH);
+    delay(2000);
+    digitalWrite(LED_2, LOW);
+  }
+
+  if( digitalRead(BOU_B_PIN) == HIGH){
+    Serial.print("Boutton B cliquer \n");
+  }
+
+  if( digitalRead(BOU_M_PIN) == HIGH){
+    Serial.print("Boutton M cliquer \n");
+  }
+
+  if( digitalRead(BOU_S_PIN) == HIGH){
+    Serial.print("Boutton S cliquer \n");
+  }
+
+  if( digitalRead(BOU_ARD_PIN) == HIGH){
+    Serial.print("Boutton ARD cliquer \n");
+  }
+  
+  if( digitalRead(BOU_ARG_PIN) == HIGH){
+    Serial.print("Boutton ARG cliquer \n");
+  }
+
+  if( analogRead(JOY_GD_PIN > 1)){
+    Serial.print("Valeur de joystick Gauche-Droit");
+    Serial.print(digitalRead(JOY_GD_PIN));
+    Serial.println();
+  }
+
+  if( analogRead(JOY_HB_PIN > 1)){
+    Serial.print("Valeur de joystick Haut-Bas");
+    Serial.print(digitalRead(JOY_HB_PIN));
+    Serial.println();
+  }
+
+
 }
