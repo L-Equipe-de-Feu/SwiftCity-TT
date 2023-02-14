f7f14ce745fcef23f52e81bf78f746272408e02c
Mat-desau <115102447+Mat-desau@users.noreply.github.com>
2023-02-13 20:43:58 -0500

    Arduino 15 (Ajout d'un debounce sur les bouttons)

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,27 @@
 #include <string.h>
 #include <ArduinoJson.h>
 
+int BOU_A = HIGH;
+int BOU_B = HIGH;
+int BOU_M = HIGH;
+int BOU_S = HIGH;
+int BOU_ARD = HIGH;
+int BOU_ARG = HIGH;
+int STATE_BOU_A = -1;
+int STATE_BOU_B = -1;
+int STATE_BOU_M = -1;
+int STATE_BOU_S = -1;
+int STATE_BOU_ARD = -1;
+int STATE_BOU_ARG = -1;
+
+long lastDebounceTime_A = 0;
+long lastDebounceTime_B = 0;
+long lastDebounceTime_M = 0;
+long lastDebounceTime_S = 0;
+long lastDebounceTime_ARD = 0;
+long lastDebounceTime_ARG = 0;
+long debouceDelay = 10;
+
 void setup()
 {
   Serial.begin(BAUD);
@@ -13,44 +34,116 @@ void setup()
 
 void loop()
 {
+  BOU_A = digitalRead(BOU_A_PIN);
+  BOU_B = digitalRead(BOU_B_PIN);
+  BOU_M = digitalRead(BOU_M_PIN);
+  BOU_S = digitalRead(BOU_S_PIN);
+  BOU_ARD = digitalRead(BOU_ARD_PIN);
+  BOU_ARG = digitalRead(BOU_ARG_PIN);
 
-  if (digitalRead(BOU_A_PIN) == LOW)
+  if ((millis() - lastDebounceTime_A) > debouceDelay)
   {
-    Serial.print("Boutton A cliquer \n");
-    Serial.println("Allumange de la LED1");
-    digitalWrite(LED_1, HIGH);
-    delay(2000);
-    digitalWrite(LED_1, LOW);
-  }
-
-  if (digitalRead(BOU_B_PIN) == LOW)
-  {
-    Serial.print("Boutton B cliquer \n");
-    Serial.print("Démarrage test de 7 segments \n");
-    for (int i = 1; i < 4; i++)
+    if ((BOU_A == LOW) && (STATE_BOU_A < 0))
     {
-      testaffichage(i);
-     }
+      lastDebounceTime_A = millis();
+      STATE_BOU_A = -STATE_BOU_A;
+
+      Serial.print("Boutton A cliquer \n");
+      //Serial.println("Allumange de la LED1");
+      //digitalWrite(LED_1, HIGH);
+      //delay(2000);
+      //digitalWrite(LED_1, LOW);
+    }
+    else if ((BOU_A == HIGH) && (STATE_BOU_A > 0))
+    {
+      lastDebounceTime_A = millis();
+      STATE_BOU_A = -STATE_BOU_A;
+    }
   }
 
-  if (digitalRead(BOU_M_PIN) == LOW)
+  if ((millis() - lastDebounceTime_B) > debouceDelay)
   {
-    Serial.print("Boutton M cliquer \n");
+    if ((BOU_B == LOW) && (STATE_BOU_B < 0))
+    {
+      lastDebounceTime_B = millis();
+      STATE_BOU_B = -STATE_BOU_B;
+
+      Serial.print("Boutton B cliquer \n");
+      Serial.print("Démarrage test de 7 segments \n");
+      for (int i = 1; i < 4; i++)
+      {
+        testaffichage(i);
+      }
+    }
+    else if ((BOU_B == HIGH) && (STATE_BOU_B > 0))
+    {
+      lastDebounceTime_B = millis();
+      STATE_BOU_B = -STATE_BOU_B;
+    }
   }
 
-  if (digitalRead(BOU_S_PIN) == LOW)
+  if ((millis() - lastDebounceTime_M) > debouceDelay)
   {
-    Serial.print("Boutton S cliquer \n");
+    if ((BOU_M == LOW) && (STATE_BOU_M < 0))
+    {
+      lastDebounceTime_M = millis();
+      STATE_BOU_M = -STATE_BOU_M;
+
+      Serial.print("Boutton M cliquer \n");
+    }
+    else if ((BOU_M == HIGH) && (STATE_BOU_M > 0))
+    {
+      lastDebounceTime_M = millis();
+      STATE_BOU_M = -STATE_BOU_M;
+    }
   }
 
-  if (digitalRead(BOU_ARD_PIN) == LOW)
+  if ((millis() - lastDebounceTime_S) > debouceDelay)
   {
-    Serial.print("Boutton ARD cliquer \n");
+    if ((BOU_S == LOW) && (STATE_BOU_S < 0))
+    {
+      lastDebounceTime_S = millis();
+      STATE_BOU_S = -STATE_BOU_S;
+
+      Serial.print("Boutton S cliquer \n");
+    }
+    else if ((BOU_S == HIGH) && (STATE_BOU_S > 0))
+    {
+      lastDebounceTime_S = millis();
+      STATE_BOU_S = -STATE_BOU_S;
+    }
   }
 
-  if (digitalRead(BOU_ARG_PIN) == LOW)
+  if ((millis() - lastDebounceTime_ARD) > debouceDelay)
   {
-    Serial.print("Boutton ARG cliquer \n");
+    if ((BOU_ARD == LOW) && (STATE_BOU_ARD < 0))
+    {
+      lastDebounceTime_ARD = millis();
+      STATE_BOU_ARD = -STATE_BOU_ARD;
+
+      Serial.print("Boutton ARD cliquer \n");
+    }
+    else if ((BOU_ARD == HIGH) && (STATE_BOU_ARD > 0))
+    {
+      lastDebounceTime_ARD = millis();
+      STATE_BOU_ARD = -STATE_BOU_ARD;
+    }
+  }
+
+  if ((millis() - lastDebounceTime_ARG) > debouceDelay)
+  {
+    if ((BOU_ARG == LOW) && (STATE_BOU_ARG < 0))
+    {
+      lastDebounceTime_ARG = millis();
+      STATE_BOU_ARG = -STATE_BOU_ARG;
+
+      Serial.print("Boutton ARG cliquer \n");
+    }
+    else if ((BOU_ARG == HIGH) && (STATE_BOU_ARG > 0))
+    {
+      lastDebounceTime_ARG = millis();
+      STATE_BOU_ARG = -STATE_BOU_ARG;
+    }
   }
 
   /*if (analogRead(JOY_GD_PIN > 1))
