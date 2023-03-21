f366dcdfe3c5af7610ba6134c26c30b75eae86fa
Mat-desau <115102447+Mat-desau@users.noreply.github.com>
2023-03-21 17:38:22 -0400

    Arduino 37 (changement send time)

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ long lastDebounceTime_ARG = 0;
 long lastDebounceTime_SEND = 0;
 
 unsigned long debouceDelay = 20;
-unsigned long SendTime = 500;
+unsigned long SendTime = 100;
 
 int Date_ref[4] = {1, 1, 2, 8};
 int Vitesse_ref[2] = {0, 1};
